ad3e4db228694d26a2fe3de7ebb2a26d9c49ff56
danielpaz98 <alexdaniel-1998@outlook.com>
2021-09-02 02:07:18 -0500

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import path from "path";
+import { defineConfig } from "vite";
+
+import vue from "@vitejs/plugin-vue";
+import eslintPlugin from "vite-plugin-eslint";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [vue(), eslintPlugin()],
+	resolve: {
+		alias: {
+			"~": path.resolve(__dirname, "src"),
+		},
+	},
+});
