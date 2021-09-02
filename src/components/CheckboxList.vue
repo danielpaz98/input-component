8bb7092425414010d37c29b33323a039a54310c0
danielpaz98 <alexdaniel-1998@outlook.com>
2021-09-02 01:50:36 -0500

    Create CheckboxList.vue

---
+++ src/components/CheckboxList.vue
@@ -0,0 +1,32 @@
+<script>
+// COMPONENTS
+import Checkbox from "~/components/Checkbox";
+
+export default {
+	name: "CheckboxList",
+};
+</script>
+
+<script setup>
+defineProps({
+	values: {
+		type: Array,
+		required: true,
+	},
+	onChange: {
+		type: Function,
+	},
+});
+</script>
+
+<template>
+	<div className="checkbox-list">
+		<Checkbox
+			v-for="({ text, value }, index) of values"
+			:key="index"
+			:label-text="text"
+			:value="value"
+			@change="onChange"
+		/>
+	</div>
+</template>
