6b10482b9bb813b79b1503fc5e720df45d7c523f
danielpaz98 <alexdaniel-1998@outlook.com>
2021-09-02 01:43:51 -0500

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+// COMPONENTS
+import Sidebar from "~/components/Sidebar";
+// VIEWS
+import ButtonContextProvider from "~/contexts/ButtonContext.vue";
+// PROVIDER CONTEXTS
+import SectionPreview from "~/views/SectionPreview.vue";
+</script>
+
+<template>
+	<main>
+		<Sidebar />
+
+		<div className="container">
+			<ButtonContextProvider>
+				<SectionPreview />
+			</ButtonContextProvider>
+		</div>
+	</main>
+</template>
