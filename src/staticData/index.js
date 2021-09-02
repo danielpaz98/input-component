907e612ec9d6d5103a24da48516de240e88f3b05
danielpaz98 <alexdaniel-1998@outlook.com>
2021-09-02 02:06:38 -0500

    Add static data for the different options for the input state

---
+++ src/staticData/index.js
@@ -0,0 +1,28 @@
+export const checkboxOptions = [
+	{ text: "Error", value: "error" },
+	{ text: "Disabled", value: "disabled" },
+	{ text: "Full width", value: "full-width" },
+];
+
+export const iconOptions = [
+	{ text: "shoping cart", value: "shopping_cart" },
+	{ text: "search", value: "search" },
+	{ text: "favorite", value: "favorite" },
+	{ text: "thumb up", value: "thumb_up" },
+	{ text: "done outline", value: "done_outline" },
+];
+
+export const colorOptions = [
+	{ text: "purple", value: "purple" },
+	{ text: "teal", value: "teal" },
+	{ text: "lime", value: "lime" },
+];
+
+export const variantOptions = [
+	{ text: "outline", value: "outline" },
+	{ text: "text", value: "text" },
+];
+
+export const sizeOptions = ["medium", "small"];
+
+export const rowOptions = ["1", "2", "3", "4"];
