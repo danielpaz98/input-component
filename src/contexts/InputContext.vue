50d7bb5242c7a5038a6f9d40b9a0f11a0d3fd33b
danielpaz98 <alexdaniel-1998@outlook.com>
2021-09-02 02:05:51 -0500

    Add global input state context

---
+++ src/contexts/InputContext.vue
@@ -0,0 +1,24 @@
+<script>
+import { reactive, provide } from "vue";
+
+export default {
+	name: "InputContextProvider",
+};
+</script>
+
+<script setup>
+const inputState = reactive({
+	"label-text": "Label",
+	placeholder: "Placeholder",
+});
+
+const setInputState = (newState) => {
+	Object.assign(inputState, newState);
+};
+
+provide("InputContext", { inputState, setInputState });
+</script>
+
+<template>
+	<slot />
+</template>
