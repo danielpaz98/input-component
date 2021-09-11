993706607351b3facd0d6f89e5a62a1cd63b96f9
danielpaz98 <alexdaniel-1998@outlook.com>
2021-09-10 19:06:17 -0500

    Add footer component and CSS styles

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+	name: "Footer",
+};
+</script>
+
+<template>
+	<div class="main-footer">
+		<span>created by</span>
+
+		<a href="https://github.com/danielpaz98" target="_blank"> danielpaz98 - </a>
+
+		<a href="https://devchallenges.io" target="_blank"> devChallenges.io </a>
+	</div>
+</template>
+
+<style lang="css" src="~/assets/css/footer.css" />
